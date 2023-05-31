04ff4a9bb5b82e8e68f12d1d6495a348c1ec9784
Michal@MacBook <michal.marusan@gmail.com>
2023-05-31 14:58:49 +0200

    test conversation length

--- src/bot.ts
+++ src/bot.ts
@@ -31,6 +31,8 @@ export class EchoBot extends ActivityHandler {
         // let prompt_old = `
         // As an advanced chatbot, your primary goal is to assist users to the best of your ability. This may involve answering questions, providing helpful information, or completing tasks based on user input. In order to effectively assist users, it is important to be detailed and thorough in your responses. Use examples and evidence to support your points and justify your recommendations or solutions.
 
+        let conversation_history_array = [];
+
         // <conversation history>
 
         // User: <user input>
@@ -67,9 +69,23 @@ export class EchoBot extends ActivityHandler {
 
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
+
+            //construct conversation history from conversation_history_array
+            let tmp_conversation_history = ""
+            if (conversation_history_array.length > 10) {
+                let N = 1; // set N to the number of elements you want to remove
+                for (let i = 0; i < N; i++) {
+                    conversation_history_array.shift(); // remove the first element from the array
+                }
+            }
+            for (let i = 0; i < conversation_history_array.length; i++) {
+                tmp_conversation_history = "<|im_start|>user " + conversation_history_array[i][0] + "<|im_end|>\n<|im_start|>assistant " + conversation_history_array[i][1] + "\n"
+            }
+            console.log(tmp_conversation_history)
             
             // construct prompt
-            let tmp_prompt = prompt.replace("<conversation history>", conversation_history).replace("<user input>", context.activity.text)
+            //let tmp_prompt = prompt.replace("<conversation history>", conversation_history).replace("<user input>", context.activity.text)
+            let tmp_prompt = prompt.replace("<conversation history>", tmp_conversation_history).replace("<user input>", context.activity.text)
             
             // construct request body
             const requestBody =     {
@@ -86,6 +102,7 @@ export class EchoBot extends ActivityHandler {
             // update conversation history
             //conversation_history = conversation_history + "User: " + context.activity.text + "\nChatbot: " + data.choices[0].text + "\n"
             conversation_history = conversation_history + "<|im_start|>user " + context.activity.text + "<|im_end|>\n<|im_start|>assistant " + data.choices[0].text + "\n"
+            conversation_history_array.push([context.activity.text,data.choices[0].text])
             // send response to user
             const replyText = `${ data.choices[0].text.replace("<|im_end|>", "") } \n[~  ${data.usage.total_tokens} tokens]`;
             // const replyText = `Echox: ${ context.activity.text } value: ${ context.activity.value }`;
